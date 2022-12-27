cdfb375ff5fda515557c2f39eb839627d309fe9f
ung <test@abc.com>
2022-12-27 17:50:59 +0700

    update

--- lib/pages/question_page.dart
+++ lib/pages/question_page.dart
@@ -1,6 +1,7 @@
 // ignore_for_file: public_member_api_docs, sort_constructors_first
 import 'dart:convert';
 
+import 'package:app_deaf/models/answer_model.dart';
 import 'package:app_deaf/models/question_model.dart';
 import 'package:app_deaf/utils/app_constant.dart';
 import 'package:app_deaf/widget/widget_image_youtube.dart';
@@ -26,6 +27,11 @@ class _QuestionPageState extends State<QuestionPage> {
   bool load = true;
   bool? haveData;
   var questionModels = <QuestionModel>[];
+  var listAnswerModels = <List<AnswerModel>>[];
+  var corrects = <int?>[];
+  var trueCorrects = <int>[];
+
+  var listCorrectBools = <List<bool>>[];
 
   @override
   void initState() {
@@ -38,7 +44,7 @@ class _QuestionPageState extends State<QuestionPage> {
     String urlAPI =
         'https://www.androidthai.in.th/fluttertraining/getQuestionWhereIdCourseUng.php?isAdd=true&coure_id=${couresModel!.id}';
 
-    await Dio().get(urlAPI).then((value) {
+    await Dio().get(urlAPI).then((value) async {
       load = false;
 
       if (value.toString() == 'null') {
@@ -48,9 +54,28 @@ class _QuestionPageState extends State<QuestionPage> {
         for (var element in json.decode(value.data)) {
           QuestionModel model = QuestionModel.fromMap(element);
           questionModels.add(model);
+          var answerModels = <AnswerModel>[];
+          var correctBools = <bool>[];
+          int indexTrue = 0;
+          String urlApiAnswer =
+              'https://www.androidthai.in.th/fluttertraining/getAnswerWhereIdQuestionId.php?isAdd=true&question_id=${model.id}';
+          await Dio().get(urlApiAnswer).then((value) {
+            for (var element in json.decode(value.data)) {
+              AnswerModel answerModel = AnswerModel.fromMap(element);
+              answerModels.add(answerModel);
+              correctBools.add(false);
+              if (answerModel.correct.isNotEmpty) {
+                trueCorrects.add(indexTrue);
+              }
+              indexTrue++;
+            }
+          });
+          listAnswerModels.add(answerModels);
+          listCorrectBools.add(correctBools);
+          corrects.add(null);
         }
       }
-
+      print('trueCorrects ---> $trueCorrects');
       setState(() {});
     });
   }
@@ -60,6 +85,12 @@ class _QuestionPageState extends State<QuestionPage> {
     return Scaffold(
       appBar: AppBar(
         title: Text(couresModel!.couresname),
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(Icons.check_box),
+          )
+        ],
       ),
       body: load
           ? const Center(child: CircularProgressIndicator())
@@ -78,8 +109,34 @@ class _QuestionPageState extends State<QuestionPage> {
                           contentQuesetion(index),
                         ],
                       ),
-                      Text('questionId ---> ${questionModels[index].id}'),
-                      Divider(color: AppConstant.darkColor, thickness: 1,)
+                      ListView.builder(
+                        shrinkWrap: true,
+                        physics: const ScrollPhysics(),
+                        itemCount: listAnswerModels[index].length,
+                        itemBuilder: (context, index2) => RadioListTile(
+                          value: index2,
+                          groupValue: corrects[index],
+                          onChanged: (value) {
+                            corrects[index] = value!;
+                            print('corrects ==> $corrects');
+                            setState(() {});
+                          },
+                          title: listAnswerModels[index][index2]
+                                  .answerText
+                                  .isNotEmpty
+                              ? Text(listAnswerModels[index][index2].answerText)
+                              : WidgetImageYoutube(
+                                  urlYoutube: listAnswerModels[index][index2]
+                                      .answerVideo,
+                                  width: 100,
+                                  height: 70,
+                                ),
+                        ),
+                      ),
+                      Divider(
+                        color: AppConstant.darkColor,
+                        thickness: 1,
+                      )
                     ],
                   ),
                 )
